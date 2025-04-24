e305830009bdba0852a9b382c81ad41821631852
bm3871 <bm3871@pleasantonusd.net>
2025-04-24 21:32:11 +0000

    nacho

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        // Test individual methods:
-        System.out.println("Random 1 - 10: " + Random.random1To10());
-        System.out.println("Random 1 to N: "  + Random.random1ToN(50));
-        System.out.println("Random X to Y: " + Random.randomXtoY(0,5));
+      // Test individual methods
+      System.out.println("Random 1 - 10: " + Random.random1To10());
+      System.out.println("Random 1 to N (N=50): "  + Random.random1ToN(50));
+      System.out.println("Random X to Y (0 to 5): " + Random.randomXtoY(0, 5));
 
-        // Test Suite
-        TestSuite.run();
+      // Run the full test suite
+      TestSuite.run();
   }
 }
--- Random.java
+++ Random.java
@@ -2,48 +2,33 @@ public class Random {
 
     // generates a random integer from 1 - 10, inclusive of both
     public static int random1To10() {
-        int rand = 0;
-        // TODO: Code here
+        int rand = (int)(Math.random() * 10) + 1;
         return rand;
     }
 
     // generates a random integer from 1 - N, inclusive of both
-    public static int random1ToN(int n)
-    {
-        int rand = 0;
-        // TODO: Code here
+    public static int random1ToN(int n) {
+        int rand = (int)(Math.random() * n) + 1;
         return rand;
     }
 
-    // generates a random integer from x to y, inclusive of both.
-    public static int randomXtoY(int x, int y)
-    {
-        int rand = 0;
-        // TODO: Code here
+    // generates a random integer from x to y, inclusive of both
+    public static int randomXtoY(int x, int y) {
+        int rand = (int)(Math.random() * (y - x + 1)) + x;
         return rand;
     }
 
-
-    // Helper - leave alone
+    // helper method used by TestSuite
     public static int getRandom(String type, int x, int y) {
-        if (type.equals("1to10"))
-        {
+        if (type.equals("1to10")) {
             return random1To10();
-        }
-        else if (type.equals("1toN"))
-        {
+        } else if (type.equals("1toN")) {
             return random1ToN(y);
-
-        }
-        else if (type.equals("XtoY"))
-        {
+        } else if (type.equals("XtoY")) {
             return randomXtoY(x, y);
-        }
-        else
-        {
+        } else {
             System.out.println("Error! Unknown type.");
+            return 0;
         }
-        return 0;
     }
-
 }
